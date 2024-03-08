0484199e5994132295623b3f2fbf26ec853252df
Mohsin Riaz <riaz.mohsin@gmail.com>
2024-03-08 11:23:16 -0500

    Fixed: Oauth redirects

--- src/pages/Google.jsx
+++ src/pages/Google.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
 const Google = () => {
     const { search } = useLocation();
+    const jwt = search.slice(1).split('=');
+    Cookies.set(jwt[0], jwt[1], { expires: 7 });
     const url = new URL(
         '/social-hub-frontend',
         import.meta.env.VITE_CLIENT_URL
     );
-    const jwt = search.slice(1).split('=');
-    Cookies.set(jwt[0], jwt[1], { expires: 7 });
+
     window.location = url;
     return <div>Redirecting...</div>;
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,37 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
-import { useDispatch, useSelector } from 'react-redux'
-import LoadingContainer from '../components/LoadingContainer'
+import { useDispatch, useSelector } from 'react-redux';
+import LoadingContainer from '../components/LoadingContainer';
 
-import { MdOutlineCancel, MdOutlineCheckCircle } from 'react-icons/md'
-import Button from '../components/Button'
-import Modal from '../components/Modal'
-import PostBox from '../components/PostBox'
-import PostsContainer from '../components/PostsContainer'
-import { getPostItems } from '../features/post/postSlice'
+import Cookies from 'js-cookie';
+import { MdOutlineCancel, MdOutlineCheckCircle } from 'react-icons/md';
+import { useLocation } from 'react-router-dom';
+import Button from '../components/Button';
+import Modal from '../components/Modal';
+import PostBox from '../components/PostBox';
+import PostsContainer from '../components/PostsContainer';
+import { getPostItems } from '../features/post/postSlice';
 
 const Home = () => {
-    const dispatch = useDispatch()
-    const { postItems, postLoading } = useSelector((store) => store.post)
-    const { userInfo, userLoading } = useSelector((store) => store.user)
+    const dispatch = useDispatch();
+    const { postItems, postLoading } = useSelector((store) => store.post);
+    const { userInfo, userLoading } = useSelector((store) => store.user);
+    const { search } = useLocation();
+    if (!!search) {
+        const jwt = search.slice(1).split('=');
+        Cookies.set(jwt[0], jwt[1], { expires: 7 });
+        const url = new URL(
+            '/social-hub-frontend',
+            import.meta.env.VITE_CLIENT_URL
+        );
+        window.location = url;
+    }
 
     useEffect(() => {
-        dispatch(getPostItems())
-    }, [])
+        dispatch(getPostItems());
+    }, []);
 
-    if (postLoading) return <LoadingContainer />
+    if (postLoading) return <LoadingContainer />;
 
     return (
         <>
@@ -55,7 +67,7 @@ const Home = () => {
             <h3>Recent Posts</h3>
             <PostsContainer postItems={postItems} />
         </>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
