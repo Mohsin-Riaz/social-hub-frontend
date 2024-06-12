e140284605e15283a9a3dbba3980becf871322b4
Mohsin Riaz <riaz.mohsin@gmail.com>
2024-06-12 11:37:55 -0400

    Comments cleanup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
-import { FcGoogle } from 'react-icons/fc';
 import { auth_login } from '../api/auth_calls';
 import { createAvatar } from '../api/avatar_calls';
 import { createPeople } from '../api/people_calls';
 
+import { FcGoogle } from 'react-icons/fc';
+
 import Button from '../components/Button';
+import FileUpload from '../components/FileUpload';
 import InputText from '../components/InputText';
 const Login = () => {
     const { userInfo } = useSelector((store) => store.user);
@@ -99,56 +101,162 @@ const Login = () => {
 
     //LOGIN
     if (loginForm)
-        alert(
-            `To use a demo account type "demo" without quotations for email and password`
-        );
-    return (
-        <section key={'login'} className="login-page">
-            <div className="button-wrapper">
-                <div className="button-wrapper_1">
-                    <Button
-                        clickHandler={clickHandler}
-                        buttonText="Login"
-                        disabled="true"
+        return (
+            <section key={'login'} className="login-page">
+                <dialog open>
+                    <p>
+                        To use a demo account type "demo" without quotations
+                        into the email and password fields!
+                    </p>
+                    <form method="dialog">
+                        <button>OK</button>
+                    </form>
+                </dialog>
+                <div className="button-wrapper">
+                    <div className="button-wrapper_1">
+                        <Button
+                            clickHandler={clickHandler}
+                            buttonText="Login"
+                            disabled="true"
+                        />
+                    </div>
+                    <div className="button-wrapper_2">
+                        <Button
+                            clickHandler={clickHandler}
+                            buttonText="Sign Up"
+                        />
+                    </div>
+                </div>
+
+                <div className="login-form">
+                    <span>{error ? 'INVALID CREDENTIALS' : ''}</span>
+                    <InputText
+                        type="text"
+                        name="email"
+                        id="email"
+                        defaultValue=""
+                        info={info}
+                        setInfo={setInfo}
+                        setError={setError}
+                    />
+                    <InputText
+                        type="text"
+                        name="password"
+                        id="password"
+                        defaultValue=""
+                        info={info}
+                        setInfo={setInfo}
+                        setError={setError}
                     />
                 </div>
-                <div className="button-wrapper_2">
-                    <Button clickHandler={clickHandler} buttonText="Sign Up" />
+                <div className="login-button">
+                    <Button clickHandler={loginHandler} buttonText="LOGIN" />
+                    or
+                    <Button
+                        buttonIcon={<FcGoogle className="icon" />}
+                        buttonText={`Google Sign-In`}
+                        clickHandler={() => getGoogleOAuthURL()}
+                    />
                 </div>
-            </div>
+            </section>
+        );
+    //SIGNUP
+    if (!loginForm)
+        return (
+            <section key={'signup'} className="signup-page">
+                <div className="button-wrapper">
+                    <div>
+                        <Button
+                            clickHandler={clickHandler}
+                            buttonText="Login"
+                        />
+                    </div>
+                    <div>
+                        <Button
+                            clickHandler={clickHandler}
+                            buttonText="Sign Up"
+                            disabled="true"
+                        />
+                    </div>
+                </div>
+                <div className="signup-form">
+                    <span>{error ? 'INVALID CREDENTIALS' : ''}</span>
 
-            <div className="login-form">
-                <span>{error ? 'INVALID CREDENTIALS' : ''}</span>
-                <InputText
-                    type="text"
-                    name="email"
-                    id="email"
-                    defaultValue=""
-                    info={info}
-                    setInfo={setInfo}
-                    setError={setError}
-                />
-                <InputText
-                    type="text"
-                    name="password"
-                    id="password"
-                    defaultValue=""
-                    info={info}
-                    setInfo={setInfo}
-                    setError={setError}
-                />
-            </div>
-            <div className="login-button">
-                <Button clickHandler={loginHandler} buttonText="LOGIN" />
-                or
-                <Button
-                    buttonIcon={<FcGoogle className="icon" />}
-                    buttonText={`Google Sign-In`}
-                    clickHandler={() => getGoogleOAuthURL()}
-                />
-            </div>
-        </section>
-    );
+                    <h3>Required</h3>
+
+                    <InputText
+                        type="text"
+                        name="email"
+                        id="email"
+                        defaultValue=""
+                        info={info}
+                        setInfo={setInfo}
+                        setError={setError}
+                    />
+                    <InputText
+                        type="text"
+                        name="password"
+                        id="password"
+                        defaultValue=""
+                        info={info}
+                        setInfo={setInfo}
+                        setError={setError}
+                    />
+
+                    <InputText
+                        type="text"
+                        name="first name"
+                        id="first_name"
+                        info={info}
+                        setInfo={setInfo}
+                        setError={setError}
+                    />
+                    <InputText
+                        type="text"
+                        name="last name"
+                        id="last_name"
+                        info={info}
+                        setInfo={setInfo}
+                        setError={setError}
+                    />
+
+                    <h3>Optional</h3>
+                    <div>
+                        <label for="Birthdate">Birthdate</label>
+                        <input
+                            type="date"
+                            id="birthdate"
+                            name="birthdate"
+                            defaultValue="1990-01-01"
+                            min="1900-01-01"
+                            max="2005-06-30"
+                            onChange={(e) => changeHandler(e)}
+                            info={info}
+                            setInfo={setInfo}
+                            setError={setError}
+                        />
+                    </div>
+                    <InputText
+                        type="text"
+                        name="address"
+                        id="address"
+                        info={info}
+                        setInfo={setInfo}
+                        setError={setError}
+                    />
+
+                    <div className="signup-fileupload">
+                        <label>avatar</label>
+                        <div className="signup-fileupload-area">
+                            <FileUpload fileUploadFunc={fileUploadFunc} />
+                        </div>
+                    </div>
+                </div>
+                <div className="signup-button">
+                    <Button clickHandler={signUpHandler} buttonText="SIGN UP" />
+                </div>
+            </section>
+        );
 };
 
 export default Login;
